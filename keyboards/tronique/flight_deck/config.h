8bae52581e4a0b951b62766461d8d41e65b5ccc8
Rex <rexblade1@gmail.com>
2025-04-02 12:04:49 -0400

    Add prototype flight deck firmware files

---
+++ keyboards/tronique/flight_deck/config.h
@@ -0,0 +1,28 @@
+
+
+/*
+ * Serial configuration
+ */
+#define SERIAL_USART_FULL_DUPLEX    // Enable full duplex operation mode.
+#define SERIAL_USART_TX_PIN	GP0     // Set gpio pins for usart
+#define SERIAL_USART_RX_PIN	GP1
+#define SERIAL_PIO_USE_PIO1         // Force the usage of PIO1 peripheral
+
+
+#define EE_HANDS
+
+
+/*
+ * Bootmagic setup
+ */
+#define BOOTMAGIC_ROW_RIGHT 6
+#define BOOTMAGIC_COLUMN_RIGHT 5
+
+
+/*
+ * Flash configuration
+ */
+#define RP2040_FLASH_GENERIC_03H
+#define EXTERNAL_FLASH_SPI_SLAVE_SELECT_PIN 56
+#define EXTERNAL_FLASH_SPI_CLOCK_DIVISOR 2
+#define EXTERNAL_FLASH_SIZE (2048 * 1024)
---
+++ keyboards/tronique/readme.md
@@ -0,0 +1,13 @@
+Placeholder readme file for Tronique LLC boards
+Author: Rexa Rearick
+
+Models: Flight Deck, more to come
+
+Community Pages: 
+
+https://discord.gg/67snqvCQQh
+https://patreon.com/tronique_llc
+
+Social Media:
+
+https://www.linkedin.com/company/tronique-llc
